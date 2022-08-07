9119adb56117746fe56757934bc14923ca9e3240
IvanKovalov <kovalevvana8@gmail.com>
2022-08-07 16:45:53 +0300

    added sonarlint

--- src/main/java/com/week2/App.java
+++ src/main/java/com/week2/App.java
@@ -2,11 +2,9 @@ package com.week2;
 
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Properties;
 
@@ -19,29 +17,29 @@ public class App
     static double generalIncrement = 0;
     static double generalMin = 0;
     static double generalMax = 0;
+    static String typeFloat = "float";
+    static String typeDouble = "double";
 
 
     public static void main( String[] args )
     {
-        String cwd = System.getProperty("user.dir");
-        System.out.println("Current working directory : " + cwd);
 
         getInfo();
 
         if(args[0].equals("int")){
             int[][] intArray = createTableInt((int)generalIncrement, (int)generalMin,(int)generalMax);
             String outString = out(intArray);
-            printFirstRow(generalIncrement,generalMin,(int)generalMax, "int");
+            System.out.print(printFirstRow(generalIncrement,generalMin,(int)generalMax, "int"));
             System.out.print(outString);
-       } else if (args[0].equals("double")) {
+       } else if (args[0].equals(typeDouble)) {
             double[][]doubleArray = createTableDouble(generalIncrement,generalMin,generalMax);
             String outString = outDouble(doubleArray);
-            printFirstRow(generalIncrement,generalMin,(int)generalMax, "double");
+            System.out.print(printFirstRow(generalIncrement,generalMin,(int)generalMax, typeDouble));
             System.out.print(outString);
-        }else if (args[0].equals("float")) {
+        }else if (args[0].equals(typeFloat)) {
             float[][] floatArray = createTableFloat((float) generalIncrement, (float) generalMin, (float) generalMax);
             String outString = outFloat(floatArray);
-            printFirstRow(generalIncrement,generalMin,(int)generalMax, "float");
+            System.out.print(printFirstRow(generalIncrement,generalMin,(int)generalMax, typeFloat));
             System.out.print(outString);
         }
 
@@ -100,7 +98,7 @@ public class App
         return  tableFloat;
     }
 
-    static String out (int[][] Array){
+    static String out (int[][] array){
         String outResult = "";
 
         int amountOfColumn = (int)generalMax/(int)generalIncrement;
@@ -108,14 +106,14 @@ public class App
         for (int i = 0; i < 10; i += 1){
             outResult = outResult + i + " ";
             for (int j = 0; j < amountOfColumn; j += 1){
-                outResult = outResult + Array[i][j]+ " ";
+                outResult = outResult + array[i][j]+ " ";
             }
             outResult = outResult + " \n";
         }
         return outResult;
     }
 
-    static String outDouble (double[][] Array){
+    static String outDouble (double[][] array){
         String outResult = "";
 
         int amountOfColumn = (int)generalMax/(int)generalIncrement;
@@ -123,14 +121,14 @@ public class App
         for (int i = 0; i < 10; i += 1){
             outResult = outResult + i + " ";
             for (int j = 0; j < amountOfColumn; j += 1){
-                outResult = outResult + Array[i][j]+ " ";
+                outResult = outResult + array[i][j]+ " ";
             }
             outResult = outResult + " \n";
         }
         return outResult;
     }
 
-    static String outFloat (float[][] Array){
+    static String outFloat (float[][] array){
         String outResult = "";
 
         int amountOfColumn = (int)generalMax/(int)generalIncrement;
@@ -138,7 +136,7 @@ public class App
         for (int i = 0; i < 10; i += 1){
             outResult = outResult + i + " ";
             for (int j = 0; j < amountOfColumn; j += 1){
-                outResult = outResult + Array[i][j]+ " ";
+                outResult = outResult + array[i][j]+ " ";
             }
             outResult = outResult + " \n";
         }
@@ -148,33 +146,33 @@ public class App
 
     static String printFirstRow(double increment, double min, int max, String format){
         String stringForReturt = " ";
-        System.out.print("  ");
+
         for (int i = 0; i < max/increment; i++){
             if (Objects.equals(format, "int")){
-                System.out.print((int)min + " ");
+
                 stringForReturt = stringForReturt + (int)min + " ";
-            } else if (Objects.equals(format, "double")) {
-                System.out.print(min + " ");
+            } else if (Objects.equals(format, typeDouble)) {
+
                 stringForReturt = stringForReturt + min + " ";
-            } else if (Objects.equals(format, "float")) {
-                System.out.print((float)min + " ");
+            } else if (Objects.equals(format, typeFloat)) {
+
                 stringForReturt = stringForReturt + min + " ";
             }
 
             min += increment;
         }
+        stringForReturt = stringForReturt + "\n";
 
-        System.out.println("");
         return  stringForReturt;
     }
 
     static void getInfo () {
 
         Properties prop = new Properties();
-        try {
-            prop.load(new InputStreamReader(new FileInputStream(System.getProperty("user.dir") + "/config.properties"), Charset.forName("UTF-8")));
+        try(InputStreamReader isr = new InputStreamReader(new FileInputStream(System.getProperty("user.dir") + "/config.properties"), StandardCharsets.UTF_8)) {
+            prop.load(isr);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("My message");
         }
         generalIncrement = Double.parseDouble(prop.getProperty("increment"));
         generalMin = Double.parseDouble(prop.getProperty("min"));
--- src/test/java/com/week2/AppTest.java
+++ src/test/java/com/week2/AppTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.*;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 
@@ -19,7 +20,7 @@ import static org.mockito.Mockito.verify;
 /**
  * Unit test for simple App.
  */
-public class AppTest
+ class AppTest
 {
     /**
      * Rigorous Test :-)
@@ -29,7 +30,7 @@ public class AppTest
 
 
     @Test
-    public void shouldAnswerWithTrue()
+    void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
@@ -39,14 +40,9 @@ public class AppTest
         System.out.println("@BeforeAll executed");
     }
 
-    @BeforeEach
-    void setupThis(){
-        System.out.println("@BeforeEach executed");
-    }
-
 
     @Test
-    public void testGetInfo()
+    void testGetInfo()
     {
 
         System.out.println("======TEST GetInfo EXECUTED=======");
@@ -58,7 +54,7 @@ public class AppTest
     }
 
     @Test
-    public void testCreateDouble()
+    void testCreateDouble()
     {
         double[][] arrayToCompare = {{0.0, 0.0},{ 1.0, 4.8},{2.0, 9.6},{3.0, 14.399999999999999},{4.0, 19.2},{5.0, 24.0},{6.0, 28.799999999999997},{7.0, 33.6},{8.0, 38.4}, { 9.0, 43.199999999999996}};
         System.out.println("======TEST createDoubleTable EXECUTED=======");
@@ -66,7 +62,7 @@ public class AppTest
     }
 
     @Test
-    public void testCreateInt()
+     void testCreateInt()
     {
 
         int[][] arrayToCompare = {{0, 0},{ 1, 4},{2, 8},{3, 12},{4, 16},{5, 20},{6, 24},{7, 28},{8, 32}, { 9, 36}};
@@ -79,24 +75,27 @@ public class AppTest
     public final SystemOutRule log = new SystemOutRule().enableLog();
 
     @Test
-    public void testPrintFirstRowInt(){
-        assertEquals(" 1 4 ", App.printFirstRow(3.8,1,6, "int"));
+     void testPrintFirstRowInt(){
+        assertEquals(" 1 4 \n", App.printFirstRow(3.8,1,6, "int"));
+        System.out.println("======TEST testPrintFirstRowInt EXECUTED=======");
     }
 
     @Test
-    public void testPrintFirstRowDouble(){
-        assertEquals(" 1.0 4.8 ", App.printFirstRow(3.8,1,6, "double"));
+     void testPrintFirstRowDouble(){
+        assertEquals(" 1.0 4.8 \n", App.printFirstRow(3.8,1,6, "double"));
+        System.out.println("======TEST testPrintFirstRowDouble EXECUTED=======");
     }
 
     @Test
-    public void testPrintFirstRowFloat(){
+     void testPrintFirstRowFloat(){
 
-        assertEquals(" 1.0 4.8 ", App.printFirstRow(3.8,1,6, "float"));
+        assertEquals(" 1.0 4.8 \n", App.printFirstRow(3.8,1,6, "float"));
+        System.out.println("======TEST testPrintFirstRowFloat EXECUTED=======");
 
     }
 
     @Test
-    public void testOutDouble(){
+     void testOutDouble(){
         App.generalMax = 6;
         App.generalMin = 1;
         App.generalIncrement = 3.8;
@@ -104,10 +103,11 @@ public class AppTest
         assertTrue(App.outDouble(doubleArray).contains("2 2.0 9.6"));
         assertTrue(App.outDouble(doubleArray).contains("3 3.0 14.399999999999999"));
         assertTrue(App.outDouble(doubleArray).contains("8 8.0 38.4"));
+        System.out.println("======TEST testPrintDouble EXECUTED=======");
     }
 
     @Test
-    public void testOutInt(){
+     void testOutInt(){
         App.generalMax = 6;
         App.generalMin = 1;
         App.generalIncrement = 3.8;
@@ -115,10 +115,11 @@ public class AppTest
         assertTrue(App.out(intArray).contains("0 0 0"));
         assertTrue(App.out(intArray).contains("5 5 20"));
         assertTrue(App.out(intArray).contains("9 9 36"));
+        System.out.println("======TEST testPrintInt EXECUTED=======");
     }
 
     @Test
-    public void testOutFloat(){
+     void testOutFloat(){
         App.generalMax = 6;
         App.generalMin = 1;
         App.generalIncrement = 3.8;
@@ -126,13 +127,9 @@ public class AppTest
         assertTrue(App.outFloat(floatArray).contains("2 2.0 9.6"));
         assertTrue(App.outFloat(floatArray).contains("6 6.0 28.800001"));
         assertTrue(App.outFloat(floatArray).contains("9 9.0 43.2"));
+        System.out.println("======TEST testPrintFloat EXECUTED=======");
     }
 
-
-    @AfterEach
-    void tearThis(){
-        System.out.println("@AfterEach executed");
-    }
 
     @AfterAll
     static void tear(){
